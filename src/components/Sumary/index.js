afd83bb18ea458983d05a83b26ebaa970740c670
Le Quoc Vinh <78015080+lequocvinh15042001@users.noreply.github.com>
2022-06-29 23:13:30 +0700

    Lần 1

--- src/components/Sumary/index.js
+++ src/components/Sumary/index.js
@@ -1,35 +1,3 @@
-// import {Grid} from '@material-ui/core';
-// import React, { useEffect, useState } from 'react';
-// import HightMaps from '../Charts/HightMaps';
-// import LineChart from '../Charts/LineChart';
-
-// export default function Sumary({report, selectedCountryId}) {
-//     const [mapData, setMapData] = useState({});
-//     useEffect(() => {
-//         if(selectedCountryId){
-//            import(`@highcharts/map-collection/countries/${selectedCountryId}/${selectedCountryId}-all.geo.json`
-//            ).then((res) => {
-//             setMapData(res);
-//            })
-//            .catch((err) => console.log({ err }));
-//         }
-//     },[selectedCountryId]);
-
-//   return (
-//     <div style={{height: '500px', marginTop: 10}}>
-//         <Grid container spacing ={3}>
-//             <Grid item sm={8} xs={12}>
-//                 <LineChart data={report} />
-//             </Grid>
-//             <Grid item sm={4} xs={12}>
-//                 <HightMaps mapData={mapData} />
-//             </Grid>
-//         </Grid>
-//     </div>
-//   );
-// }
-
-
 import React, { useEffect, useState } from 'react';
 import Grid from '@material-ui/core/Grid';
 
